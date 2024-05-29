9d1036177b2e48eea3ea719ae8f395a74fcf7487
Abhitachi <abhishekshetter1999@gmail.com>
2024-05-29 20:56:50 +0530

    added spaceX api and populated the data

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 
 const { mongoConnect } = require("../src/services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
-
+const { loadLaunchData } = require("./models/launches.model");
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
@@ -12,6 +12,7 @@ async function startServer() {
   await mongoConnect();
 
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
